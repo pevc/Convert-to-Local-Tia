5bc40020670168d46987587184704d1fb39ee207
Fernando Teixeira <fernandoteixeiramarques@gmail.com>
2023-06-09 12:20:25 -0300

    adicionando versão código 0.1

--- BS4.py
+++ BS4.py
@@ -1,6 +1,5 @@
 import os
 import re
-import Baixar_Videos
 from bs4 import BeautifulSoup
 
 i = 0
@@ -34,31 +33,9 @@ def convert_point_local(file_tag):
     #print(f"Arquivo '{nome_arquivo}' salvo com sucesso na pasta '{pasta}'.")    
     
      
+    
 
-def convert_drive_videos(tag_video) :
-    tag_video_str = str(tag_video)
-    if tag_video_str != "[]" and "drive" in tag_video_str :
-            #print(file_name)
-            convert_point_local(tag_video_str)
 
-            with open(point_local, 'r') as file:
-                point_local_data = file.read()
-
-            #print(point_local_data)
-
-            #print(tag_video.prettify())
-            #tag_video.div.parent.replace_with(str(point_local_data))
-            tag_video.div.parent.replace_with(BeautifulSoup(point_local_data, "html.parser").div)
-            
-            #print(tag_video.prettify())
-            # Salve o HTML modificado de volta no arquivo
-            #with open(file_path, 'w') as file:
-               #file.write(soup.prettify())
-
-def convert_youtube_videos(tag_video) :
-    tag_video_str = str(tag_video)
-    if tag_video_str != "[]" and "youtube" in tag_video_str:
-        print()
 
 folder_path = "DRAFT"     
 # Procurando todos os diretorios
@@ -74,21 +51,23 @@ for file_name in os.listdir(folder_path):
         
         soup = BeautifulSoup(file_data, "html.parser")
 
-        # tag_video para drive
-        tag_videos = soup.find_all('div',class_ = "WIdY2d M1aSXe")
+        tag_video = soup.find('div',class_ = "WIdY2d M1aSXe")
+        tag_video_str = str(tag_video)
 
-        for tag_video in tag_videos :
-            print("\n\n")
-            print(file_name)
-            print("\n\n\n\n\n\n")
-            print(tag_video.prettify())
-            #convert_drive_videos(tag_video)
-            #convert_youtube_videos(tag_video)
+        if tag_video_str != "[]" and "drive" in tag_video_str :
+            #print(file_name)
+            convert_point_local(tag_video_str)
 
-        
+            with open(point_local, 'r') as file:
+                point_local_data = file.read()
 
             #print(point_local_data)
-            # new_tag = soup.new_tag("b")
-            # new_tag.string = "example.com"
-            # print(type(new_tag))
-            # print(type(tag_video)
+
+            #print(tag_video.prettify())
+            #tag_video.div.parent.replace_with(str(point_local_data))
+            tag_video.div.parent.replace_with(BeautifulSoup(point_local_data, "html.parser").div)
+            
+            #print(tag_video.prettify())
+            # Salve o HTML modificado de volta no arquivo
+            with open(file_path, 'w') as file:
+                file.write(soup.prettify())
